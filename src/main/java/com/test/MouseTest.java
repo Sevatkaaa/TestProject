80cf81629683a490fe1a2e22b4582c99cab576b3
vsevolod.reshetnikov <ifq5n4vafda33wiebc3vwjeyc6sk4g6d5sbrpubh347b2red6e4a>
2022-06-09 07:35:34 +0300

    fix mouse test

--- src/main/java/com/test/MouseTest.java
+++ src/main/java/com/test/MouseTest.java
@@ -1,18 +1,21 @@
 package com.test;
 
 import java.awt.Robot;
+import java.awt.event.InputEvent;
 import java.util.Random;
 
 public class MouseTest {
     public static final int FIVE_SECONDS = 5000;
-    public static final int MAX_Y = 400;
-    public static final int MAX_X = 400;
+    public static final int MAX_Y = 50;
+    public static final int MAX_X = 50;
 
     public static void main(String... args) throws Exception {
         Robot robot = new Robot();
         Random random = new Random();
         while (true) {
-            robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
+            robot.mouseMove(random.nextInt(MAX_X) + 800, random.nextInt(MAX_Y) + 800);
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
             Thread.sleep(FIVE_SECONDS);
         }
     }
